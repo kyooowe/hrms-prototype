0be1fa4d0c75f843dadff31cfd30a24da9c7ceb3
kyooowe <brinasjohnmark@gmail.com>
2024-06-21 11:56:20 +0800

    add employee dashboard, add darkmode toggle

---
+++ src/app/(pages)/employees/page.tsx
@@ -0,0 +1,215 @@
+//#region Import
+import {
+    ChevronLeft,
+    ChevronRight,
+    File,
+    ListFilter,
+    Pencil,
+    Trash,
+} from "lucide-react"
+import { Badge } from "@/components/ui/badge"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    DropdownMenu,
+    DropdownMenuCheckboxItem,
+    DropdownMenuContent,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from "@/components/ui/table"
+import {
+    Tabs,
+    TabsContent,
+    TabsList,
+    TabsTrigger,
+} from "@/components/ui/tabs"
+import MainLayout from "@/components/layout/main"
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem
+} from "@/components/ui/pagination"
+//#endregion
+
+const Dashboard = () => {
+    return (
+        <MainLayout>
+            <Tabs defaultValue="week">
+                <div className="flex items-center">
+                    <TabsList>
+                        <TabsTrigger value="week">Week</TabsTrigger>
+                        <TabsTrigger value="month">Month</TabsTrigger>
+                        <TabsTrigger value="year">Year</TabsTrigger>
+                    </TabsList>
+                    <div className="ml-auto flex items-center gap-2">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button
+                                    variant="outline"
+                                    size="sm"
+                                    className="h-7 gap-1 text-sm"
+                                >
+                                    <ListFilter className="h-3.5 w-3.5" />
+                                    <span className="sr-only sm:not-sr-only">Filter</span>
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent align="end">
+                                <DropdownMenuLabel>Filter by</DropdownMenuLabel>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuCheckboxItem checked>
+                                    Regular
+                                </DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem>
+                                    Probitionary
+                                </DropdownMenuCheckboxItem>
+                                <DropdownMenuCheckboxItem>
+                                    Resigned
+                                </DropdownMenuCheckboxItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                        <Button
+                            size="sm"
+                            variant="outline"
+                            className="h-7 gap-1 text-sm"
+                        >
+                            <File className="h-3.5 w-3.5" />
+                            <span className="sr-only sm:not-sr-only">Export</span>
+                        </Button>
+                        <Button
+                            size="sm"
+                            variant="outline"
+                            className="h-7 gap-1 text-sm"
+                        >
+                            <File className="h-3.5 w-3.5" />
+                            <span className="sr-only sm:not-sr-only">New Employee</span>
+                        </Button>
+                    </div>
+                </div>
+                <TabsContent value="week">
+                    <Card x-chunk="dashboard-05-chunk-3">
+                        <CardHeader className="px-7">
+                            <CardTitle>Employees</CardTitle>
+                            <CardDescription>
+                                Displaying Most Recent Entries First.
+                            </CardDescription>
+                        </CardHeader>
+                        <CardContent>
+                            <Table>
+                                <TableHeader>
+                                    <TableRow>
+                                        <TableHead>Employee</TableHead>
+                                        <TableHead className="hidden md:table-cell">
+                                            Job
+                                        </TableHead>
+                                        <TableHead className="hidden md:table-cell">
+                                            Status
+                                        </TableHead>
+                                        <TableHead className="hidden md:table-cell">
+                                            Date Hired
+                                        </TableHead>
+                                        <TableHead className=""></TableHead>
+                                    </TableRow>
+                                </TableHeader>
+                                <TableBody>
+                                    <TableRow className="bg-accent">
+                                        <TableCell>
+                                            <div className="font-medium">Jane Doe</div>
+                                            <div className="hidden text-sm text-muted-foreground md:inline">
+                                                janedoe@example.com
+                                            </div>
+                                        </TableCell>
+                                        <TableCell className="hidden sm:table-cell">
+                                            Software Developer
+                                        </TableCell>
+                                        <TableCell className="hidden sm:table-cell">
+                                            <Badge className="text-xs" variant="secondary">
+                                                Regular
+                                            </Badge>
+                                        </TableCell>
+                                        <TableCell className="hidden md:table-cell">
+                                            2023-06-23
+                                        </TableCell>
+                                        <TableCell className="flex items-center justify-center gap-2">
+                                            <Button variant="destructive" size="icon">
+                                                <Trash className="h-4 w-4" />
+                                            </Button>
+                                            <Button variant="outline" size="icon">
+                                                <Pencil className="h-4 w-4" />
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>
+                                            <div className="font-medium">John Doe</div>
+                                            <div className="hidden text-sm text-muted-foreground md:inline">
+                                                johndoe@example.com
+                                            </div>
+                                        </TableCell>
+                                        <TableCell className="hidden sm:table-cell">
+                                            Software Analyst
+                                        </TableCell>
+                                        <TableCell className="hidden sm:table-cell">
+                                            <Badge className="text-xs" variant="secondary">
+                                                Probitionary
+                                            </Badge>
+                                        </TableCell>
+                                        <TableCell className="hidden md:table-cell">
+                                            2023-06-23
+                                        </TableCell>
+                                        <TableCell className="flex items-center justify-center gap-2">
+                                            <Button variant="destructive" size="icon">
+                                                <Trash className="h-4 w-4" />
+                                            </Button>
+                                            <Button variant="outline" size="icon">
+                                                <Pencil className="h-4 w-4" />
+                                            </Button>
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </CardContent>
+                        <CardFooter className="flex flex-row items-center border-t bg-muted/50 px-6 py-3">
+                            <div className="text-xs text-muted-foreground">
+                                Updated <time dateTime="2023-11-23">June 06, 2024</time>
+                            </div>
+                            <Pagination className="ml-auto mr-0 w-auto">
+                                <PaginationContent>
+                                    <PaginationItem>
+                                        <Button size="icon" variant="outline" className="h-6 w-6">
+                                            <ChevronLeft className="h-3.5 w-3.5" />
+                                            <span className="sr-only">Previous Order</span>
+                                        </Button>
+                                    </PaginationItem>
+                                    <PaginationItem>
+                                        <Button size="icon" variant="outline" className="h-6 w-6">
+                                            <ChevronRight className="h-3.5 w-3.5" />
+                                            <span className="sr-only">Next Order</span>
+                                        </Button>
+                                    </PaginationItem>
+                                </PaginationContent>
+                            </Pagination>
+                        </CardFooter>
+                    </Card>
+                </TabsContent>
+            </Tabs>
+        </MainLayout>
+    )
+}
+
+export default Dashboard
---
+++ src/components/layout/main.tsx
@@ -0,0 +1,179 @@
+"use client"
+
+//#region Import
+import Image from "next/image"
+import Link from "next/link"
+import {
+    Home,
+    Moon,
+    Package2,
+    Search,
+    Settings,
+    Sun,
+    Users,
+} from "lucide-react"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+import { Button } from "@/components/ui/button"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Input } from "@/components/ui/input"
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+} from "@/components/ui/tooltip"
+import { ReactNode, useEffect, useState } from "react"
+import { useTheme } from "next-themes"
+//#endregion
+
+interface MainLayoutProps {
+    children: ReactNode;
+}
+
+const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+
+    const { setTheme } = useTheme()
+    const [isDarkTheme, setIsDarkTheme] = useState(false)
+
+    useEffect(() => {
+        setTheme(isDarkTheme ? 'dark' : 'light')
+    }, [isDarkTheme])
+
+    return (
+        <div className="flex min-h-screen w-full flex-col bg-muted/40">
+            <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+                <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
+                    <Link
+                        href="#"
+                        className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+                    >
+                        <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+                        <span className="sr-only">HRMS Prototype</span>
+                    </Link>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link
+                                href="#"
+                                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                            >
+                                <Home className="h-5 w-5" />
+                                <span className="sr-only">Dashboard</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Dashboard</TooltipContent>
+                    </Tooltip>
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link
+                                href="#"
+                                className="flex h-9 w-9 items-center justify-center rounded-lg bg-accent text-accent-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                            >
+                                <Users className="h-5 w-5" />
+                                <span className="sr-only">Employees</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Employees</TooltipContent>
+                    </Tooltip>
+                </nav>
+                <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-5">
+                    <Tooltip>
+                        <TooltipTrigger asChild>
+                            <Link
+                                href="#"
+                                className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                            >
+                                <Settings className="h-5 w-5" />
+                                <span className="sr-only">Settings</span>
+                            </Link>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">Settings</TooltipContent>
+                    </Tooltip>
+                </nav>
+            </aside>
+            <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+                <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+                    <Breadcrumb className="hidden md:flex">
+                        <BreadcrumbList>
+                            <BreadcrumbItem>
+                                <BreadcrumbLink asChild>
+                                    <Link href="#">Under</Link>
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem>
+                                <BreadcrumbLink asChild>
+                                    <Link href="#">Construction</Link>
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            <BreadcrumbSeparator />
+                            <BreadcrumbItem>
+                                <BreadcrumbPage>Bread Crumbs</BreadcrumbPage>
+                            </BreadcrumbItem>
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                    <div className="relative ml-auto flex-1 md:grow-0">
+                        <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+                        <Input
+                            type="search"
+                            placeholder="Search..."
+                            className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[336px]"
+                        />
+                    </div>
+                    <Button variant="ghost" size="icon" onClick={() => setIsDarkTheme(!isDarkTheme)}>
+                        {
+                            isDarkTheme ? (
+                                <Sun className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                            ) : (
+                                <Moon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                            )
+                        }
+                        <span className="sr-only">Toggle theme</span>
+                    </Button>
+                    <DropdownMenu>
+                        <DropdownMenuTrigger asChild>
+                            <Button
+                                variant="outline"
+                                size="icon"
+                                className="overflow-hidden rounded-full"
+                            >
+                                <Image
+                                    src="/kafka.jpg"
+                                    width={36}
+                                    height={36}
+                                    alt="Avatar"
+                                    className="overflow-hidden rounded-full"
+                                />
+                            </Button>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent align="end">
+                            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Settings</DropdownMenuItem>
+                            <DropdownMenuItem>Support</DropdownMenuItem>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>Logout</DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                </header>
+                <main className="p-4">
+                    {children}
+                </main>
+            </div>
+        </div>
+    )
+}
+
+export default MainLayout
